952e4808933cf08476103afeebcf404c91d19702
Kangjun Heo <knowledge@o.cnu.ac.kr>
2023-03-09 21:18:40 +0900

    stash task 2023/3/9

--- PEngine/ViewModels/GlobalViewModel.cs
+++ PEngine/ViewModels/GlobalViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace PEngine.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+
+namespace PEngine.ViewModels
 {
-    public class GlobalViewModel
+    public partial class GlobalViewModel : PageModel
     {
+        public bool IsLoading { get; private set; }
+
+        private CancellationTokenSource LoadingTokenSource;
+
+        public GlobalViewModel() 
+        {
+            LoadingTokenSource = new CancellationTokenSource();
+        }
+
+        public async Task RequestTask(Func<CancellationToken, Task> action)
+        {
+            IsLoading = true;
+
+            var token = LoadingTokenSource.Token;
+
+            token.ThrowIfCancellationRequested();
+            await action(token);
+
+            IsLoading = false;
+        }
     }
 }
