912d03a1f6be7d6c6699be05c3a369974aa7d9dd
Kangjun Heo <knowledge@o.cnu.ac.kr>
2023-03-12 21:41:44 +0900

    add nunit testing project

---
+++ PEngine.NUnit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace PEngine.NUnit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
