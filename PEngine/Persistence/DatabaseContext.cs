7de121fdc81f3f99ae57c0114c33e8684ec6388f
Kangjun Heo <knowledge@o.cnu.ac.kr>
2023-03-30 18:03:18 +0900

    remove unnecessary relation

--- PEngine/Persistence/DatabaseContext.cs
+++ PEngine/Persistence/DatabaseContext.cs
@@ -10,7 +10,6 @@ public class DatabaseContext : DbContext
     public DbSet<Comment>? Comments { get; set; }
     public DbSet<Post>? Posts { get; set; }
     public DbSet<User>? Users { get; set; }
-    public DbSet<UserCredentials>? UserCredentials { get; set; }
 
     public DatabaseContext()
     {
@@ -20,16 +19,16 @@ public class DatabaseContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         var connString = Program.WebsiteConfiguration?["Persistence:Database:ConnectionString"];
-        optionsBuilder.UseMySql(connString, 
+        optionsBuilder.UseMySql(connString,
             ServerVersion.Create(Version.Parse("8.0"), ServerType.MySql));
-        
+
         base.OnConfiguring(optionsBuilder);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasCharSet("utf8mb4");
-        
+
         base.OnModelCreating(modelBuilder);
     }
 }
