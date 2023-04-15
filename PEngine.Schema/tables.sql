1dc0fbcea13d78f1e2859aa78e965d9289776ed9
Kangjun Heo <kheo@sidel.tech>
2023-04-15 13:00:59 +0900

    complete schema redefinition

--- PEngine.Schema/tables.sql
+++ PEngine.Schema/tables.sql
@@ -1,29 +1,30 @@
-CREATE TABLE "Users" (
+CREATE TABLE IF NOT EXISTS "Users" (
     "Id"        uuid,
-    "Group"     uuid default "Id",
+    "Group"     uuid,
     "Username"  varchar(32),
     "DisplayName" varchar(32),
-    "Email"     varchar(128),
-    "PasswordHash" varchar(256),
-    "PasswordSalt" varchar(256),
-    "ProfileImage" uuid,
-    "Signature" uuid,
+    "Email"     varchar(128) NOT NULL,
+    "PasswordHash" varchar(256) NOT NULL,
+    "PasswordSalt" varchar(256) NOT NULL,
+    "ProfileImage" uuid, -- from "Files"
+    "Signature" uuid,    -- from "Posts"
 
-    PRIMARY KEY ("Id")
+    PRIMARY KEY ("Id"),
+
+    CONSTRAINT "Username_Unique" UNIQUE ("Username"),
+    CONSTRAINT "Email_Unique" UNIQUE ("Email")
 );
 
-CREATE TABLE "Credentials" (
+CREATE TABLE IF NOT EXISTS "Credentials" (
     "Id"        uuid,
     "Service"   varchar(32),
     "CredentialHash" varchar(256),
     "CredentialSalt" varchar(256),
 
-    CONSTRAINT "Credentials_Service_Unique" UNIQUE ("Id", "Service"),
-    CONSTRAINT "Credentials_Id_Users" FOREIGN KEY ("Id")
-            REFERENCES "Users"("Id") ON DELETE CASCADE
+    CONSTRAINT "Credentials_Service_Unique" UNIQUE ("Id", "Service")
 );
 
-CREATE TABLE Entries (
+CREATE TABLE IF NOT EXISTS "Entries" (
     "Id"        uuid,
     "Parent"    uuid,
 
@@ -44,17 +45,13 @@ CREATE TABLE Entries (
     "IPAddress" integer,
 
     PRIMARY KEY ("Id"),
-    CONSTRAINT "Entries_Unique_Name" UNIQUE ("Parent", "Name"),
-
-    CONSTRAINT "Entries_Owner_User" FOREIGN KEY ("Owner")
-        REFERENCES "Users"("Id") ON DELETE SET NULL,
-    CONSTRAINT "Entries_Parent" FOREIGN KEY ("Parent")
-        REFERENCES "Entries"("Id") ON DELETE CASCADE
+    CONSTRAINT "Entries_Unique_Name" UNIQUE ("Parent", "Name")
 );
 
-CREATE INDEX "Entries_Parent_Index" ON "Entries" USING BTREE ("Parent");
+CREATE INDEX IF NOT EXISTS "Entries_Parent_Index"
+    ON "Entries" USING BTREE ("Parent");
 
-CREATE TABLE Keyrings (
+CREATE TABLE IF NOT EXISTS "Keyrings" (
     "Id"        uuid,
     "Salt"      varchar(256), -- Used for Password to Key Derivation
     "Algorithm" varchar(32), -- Encryption Symmetric Algorithm
@@ -64,7 +61,7 @@ CREATE TABLE Keyrings (
     PRIMARY KEY ("Id")
 );
 
-CREATE TABLE Posts (
+CREATE TABLE IF NOT EXISTS "Posts" (
     "Entry"     uuid NOT NULL,
     "Author"    uuid,
     "Content"   TEXT,
@@ -72,15 +69,10 @@ CREATE TABLE Posts (
     "Encrypted" BOOLEAN default false,
     "Keyring"   uuid,
 
-    CONSTRAINT "Posts_Entry_Id" FOREIGN KEY ("Entry")
-                   REFERENCES "Entries"("Id") ON DELETE CASCADE,
-    CONSTRAINT "Posts_Author_Id" FOREIGN KEY ("Author")
-                   REFERENCES "Users"("Id") ON DELETE RESTRICT,
-    CONSTRAINT "Posts_Keyring" FOREIGN KEY ("Keyring")
-                   REFERENCES "Keyrings"("Id") ON DELETE RESTRICT
+    CONSTRAINT "Posts_Entry_Unique" UNIQUE ("Entry")
 );
 
-CREATE TABLE Comments (
+CREATE TABLE IF NOT EXISTS "Comments" (
     "Entry"     uuid NOT NULL,
     "Author"    uuid,
 
@@ -89,15 +81,13 @@ CREATE TABLE Comments (
 
     "Content"   varchar(500),
 
-    CONSTRAINT "Comments_Entry_Id" FOREIGN KEY ("Entry")
-                   REFERENCES "Entries"("Id") ON DELETE CASCADE,
-    CONSTRAINT "Comments_Author_Id" FOREIGN KEY ("Author")
-                   REFERENCES "Users"("Id") ON DELETE RESTRICT,
-    CONSTRAINT "Posts_Keyring" FOREIGN KEY ("Keyring")
-                   REFERENCES "Keyrings"("Id") ON DELETE RESTRICT
+    "Encrypted" BOOLEAN default false,
+    "Keyring"   uuid,
+
+    CONSTRAINT "Comments_Entry_Unique" UNIQUE ("Entry")
 );
 
-CREATE TABLE Files (
+CREATE TABLE IF NOT EXISTS "Files" (
     "Entry"     uuid NOT NULL,
 
     "HashName"  varchar(32),
@@ -105,9 +95,46 @@ CREATE TABLE Files (
 
     "Filetype"  varchar(64),
 
-    "Service"   varchar(32), -- filesystem or ipfs, s3, or ...
+    "Service"   varchar(32), -- filesystem when null or ipfs, s3, or ...
     "Path"      varchar(256),
 
-    CONSTRAINT "Files_Entry_Id" FOREIGN KEY ("Entry")
-                   REFERENCES "Entries"("Id") ON DELETE CASCADE,
+    CONSTRAINT "Files_Entry_Unique" UNIQUE ("Entry")
 );
+
+-- CONFIGURE FOREIGN KEYS -------------------------
+
+ALTER TABLE "Users" ADD FOREIGN KEY ("ProfileImage")
+            REFERENCES "Files"("Entry") ON DELETE SET NULL;
+
+ALTER TABLE "Users" ADD FOREIGN KEY ("Signature")
+            REFERENCES "Posts"("Entry") ON DELETE SET NULL;
+
+ALTER TABLE "Credentials" ADD FOREIGN KEY ("Id")
+            REFERENCES "Users"("Id") ON DELETE CASCADE;
+
+ALTER TABLE "Entries" ADD FOREIGN KEY ("Owner")
+        REFERENCES "Users"("Id") ON DELETE SET NULL;
+
+ALTER TABLE "Entries" ADD FOREIGN KEY ("Parent")
+        REFERENCES "Entries"("Id") ON DELETE CASCADE;
+
+ALTER TABLE "Posts" ADD FOREIGN KEY ("Entry")
+                   REFERENCES "Entries"("Id") ON DELETE CASCADE;
+
+ALTER TABLE "Posts" ADD FOREIGN KEY ("Author")
+                   REFERENCES "Users"("Id") ON DELETE RESTRICT;
+
+ALTER TABLE "Posts" ADD FOREIGN KEY ("Keyring")
+                   REFERENCES "Keyrings"("Id") ON DELETE RESTRICT;
+
+ALTER TABLE "Comments" ADD FOREIGN KEY ("Entry")
+                   REFERENCES "Entries"("Id") ON DELETE CASCADE;
+
+ALTER TABLE "Comments" ADD FOREIGN KEY ("Author")
+                   REFERENCES "Users"("Id") ON DELETE RESTRICT;
+
+ALTER TABLE "Comments" ADD FOREIGN KEY ("Keyring")
+                   REFERENCES "Keyrings"("Id") ON DELETE RESTRICT;
+
+ALTER TABLE "Files" ADD FOREIGN KEY ("Entry")
+                   REFERENCES "Entries"("Id") ON DELETE CASCADE;
