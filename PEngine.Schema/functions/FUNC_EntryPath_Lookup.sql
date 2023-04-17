02aecde6430b24370dee72cb1067bdfb95ebd007
Kangjun Heo <kheo@sidel.tech>
2023-04-17 16:55:25 +0900

    Child Lookup for a Specific Entry Path

---
+++ PEngine.Schema/functions/FUNC_EntryPath_Lookup.sql
@@ -0,0 +1,44 @@
+DROP TYPE IF EXISTS "EntryPathItem" CASCADE;
+
+CREATE TYPE "EntryPathItem" AS (
+    "Id" UUID, "Type" INTEGER, "UPerm" INTEGER, "GPerm" INTEGER, "OPerm" INTEGER,
+    "Name" VARCHAR(300), "Created" TIMESTAMP, "Modified" TIMESTAMP,
+    "CreatorId" UUID, "CreatorName" VARCHAR(32), "OwnerId" UUID, "OwnerName" VARCHAR(32),
+    "Hidden" BOOLEAN, "IpAddress" varchar(32)
+    );
+
+DROP FUNCTION IF EXISTS "EntryPath_Lookup"(p_path TEXT);
+
+CREATE OR REPLACE FUNCTION "EntryPath_Lookup"(
+        p_path TEXT
+) RETURNS SETOF "EntryPathItem" AS $$
+DECLARE
+    target_item  uuid;
+BEGIN
+    target_item := (SELECT "Id"
+                FROM "EntryPath_StrToChain"(p_path)
+                ORDER BY "Index" DESC LIMIT 1);
+
+    IF target_item IS NULL THEN
+        RETURN;
+    END IF;
+
+    RETURN QUERY SELECT
+        et."Id",
+            cast((cast(et."Mode" as bit(16)) >> 9) as INTEGER) As "Type",
+            cast((cast(et."Mode" as bit(16)) & B'0000000111000000') >> 6 as INTEGER)  As "UPerm",
+            cast((cast(et."Mode" as bit(16)) & B'0000000000111000') >> 3 as INTEGER)  As "GPerm",
+            cast((cast(et."Mode" as bit(16)) & B'0000000000000111') as INTEGER) As "OPerm",
+            et."Name", et."Created", et."Modified",
+            ut."Id" As "CreatorId", ut."DisplayName" As "CreatorName",
+            ut2."Id" As "OwnerId", ut2."DisplayName" As "OwnerName",
+            et."Hidden", et."IpAddress"
+        FROM "Entries" et
+        LEFT JOIN "Users" ut ON et."Creator" = ut."Id"
+        LEFT JOIN "Users" ut2 ON et."Owner" = ut2."Id"
+        WHERE
+            et."Parent" = target_item;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT * FROM "EntryPath_Lookup"('/usr');
--- PEngine.Schema/functions/FUNC_EntryPath_StrToChain.sql
+++ PEngine.Schema/functions/FUNC_EntryPath_StrToChain.sql
@@ -1,7 +1,7 @@
 DROP TYPE IF EXISTS "EntryPathChainNode" CASCADE;
 
 CREATE TYPE "EntryPathChainNode" AS (
-    "Id" UUID, "Parent" UUID, "Name" VARCHAR(300) );
+    "Index" INTEGER, "Id" UUID, "Parent" UUID, "Name" VARCHAR(300) );
 
 DROP FUNCTION IF EXISTS "EntryPath_StrToChain"(p_path TEXT);
 
@@ -12,39 +12,45 @@ DECLARE
     current_entry  RECORD;
     current_path   VARCHAR(300);
     current_parent UUID;
+    index          INTEGER;
 BEGIN
+    index := 0;
+
     CREATE TEMPORARY TABLE result (
-        "Id" UUID, "Parent" UUID, "Name" VARCHAR(300) )
+        "Index" INTEGER, "Id" UUID, "Parent" UUID, "Name" VARCHAR(300) )
     ON COMMIT DROP;
 
-    path_elements := string_to_array(rtrim(ltrim(p_path, '/'), '/'), '/');
-    current_parent := NULL;
+    path_elements := string_to_array(ltrim(rtrim(p_path, '/'), '/'), '/');
+    current_parent := '00000000-0000-0000-0000-000000000000'::uuid;
+
+    IF path_elements = '{}' THEN
+        RETURN QUERY SELECT
+                         0::INTEGER           As "Index",
+                         current_parent::uuid As "Id",
+                         NULL::uuid           As "Parent",
+                         ''::VARCHAR(300)     As "Name";
+    END IF;
 
     RAISE NOTICE '%', path_elements;
 
     FOREACH current_path IN ARRAY path_elements
     LOOP
-        IF current_parent IS NULL THEN
-            SELECT "Id", "Parent", "Name"
-                INTO current_entry
-                FROM "Entries"
-                WHERE "Name" = current_path;
-        ELSE
-            SELECT "Id", "Parent", "Name"
-                INTO current_entry
-                FROM "Entries"
-                WHERE "Parent" = current_parent AND "Name" = current_path;
-        END IF;
-
-        RAISE NOTICE '%,%,%:%', current_path, current_entry, current_parent, current_entry IS NULL;
+        SELECT "Id", "Parent", "Name"
+            INTO current_entry
+            FROM "Entries"
+            WHERE "Parent" = current_parent AND "Name" = current_path;
 
         IF current_entry IS NULL THEN
             RETURN;
         END IF;
 
         current_parent := current_entry."Id";
+
         INSERT INTO result (
-            SELECT current_entry."Id", current_entry."Parent", current_entry."Name");
+            SELECT index As "Index", current_entry."Id", current_entry."Parent", current_entry."Name");
+
+        index := index + 1;
+
     END LOOP;
 
     RETURN QUERY SELECT * FROM result;
