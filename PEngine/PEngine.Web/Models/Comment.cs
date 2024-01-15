816109252e85fb8be93509433fa74867bda573f7
Kangjun Heo <kheo@sidelcorp.com>
2024-01-15 19:44:05 +0900

    richer comment entity

--- PEngine/PEngine.Web/Models/Comment.cs
+++ PEngine/PEngine.Web/Models/Comment.cs
@@ -6,13 +6,21 @@ namespace PEngine.Web.Models
     {
         [Key]
         public Guid Id { get; set; }
+
+        public Guid? ReplyOf { get; set; }
+        
         public Guid? WrittenBy { get; set; }
+        
         public long Post { get; set; }
         public string? WriterName { get; set; }
-        public string? WriterIp { get; set; }
+        public long WriterIp { get; set; }
         public string? WriterUserAgent { get; set; }
         public string? Password { get; set; }
         public string? Content { get; set; }
-        public DateTime WrittenAt { get; set; } = DateTime.Now;
+        public long WrittenAt { get; set; } = (long) DateTime.Now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+
+        public bool IsFixed { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsHidden { get; set; }
     }
 }
